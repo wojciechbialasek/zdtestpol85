be5c9bc98653132d795a558bccd0f762adc6b845
Wojtek <wojciu01@gmail.com>
2021-10-22 19:16:15 +0200

    Kolejny commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world 2021");
         System.out.println("Polskie wybory 2020!!");
     }
 }
