9a92fbb6c783910d03bbf16e8fb72d934e371d08
Wojtek <wojciu01@gmail.com>
2021-10-23 09:42:05 +0200

    Casdfasdfasdommit pierwsze deva

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world 2020");
+        System.out.println("Hello world 2022");
     }
 
    private void displayText() {
