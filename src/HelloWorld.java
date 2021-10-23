25679e27904e0637e4620a8b03d3fd88fecc6abe
Wojtek <wojciu01@gmail.com>
2021-10-23 09:17:53 +0200

    Npowy commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world 2021");
+        System.out.println("Hello world 2021 I 2022");
     }
 
    private void displayText() {
